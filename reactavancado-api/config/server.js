ab216b46550a6b150c4e1b748457e734b2c6110f
MatheusEstanislau <matheus.estanislau7@gmail.com>
2021-01-18 06:50:20 -0300

    Adicionado strapi

---
+++ reactavancado-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '35129e893f16a60b3984a57d78851271'),
+    },
+  },
+});
---
+++ reactavancado-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f0d23840-f366-4270-93ff-91ce035423b0'
+};
