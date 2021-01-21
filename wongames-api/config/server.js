2bd58ea3942779c1e6691fb5ce37ba65db4bcc8d
MatheusEstanislau <matheus.estanislau7@gmail.com>
2021-01-21 09:33:39 -0300

    Iniciado strapi-app wongames

---
+++ wongames-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c74b18f655ac7e399aa02689daac22ce'),
+    },
+  },
+});
