5f464d0601ca65ab94a8aafb9bb3a1650c607980
MatheusEstanislau <matheus.estanislau7@gmail.com>
2021-01-24 19:33:00 -0300

    Criado componente Logo

---
+++ client/src/utils/tests/helper.tsx
@@ -0,0 +1,6 @@
+import { render, RenderResult } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+import theme from 'styles/theme'
+
+export const renderWithTheme = (Children: React.ReactNode): RenderResult =>
+  render(<ThemeProvider theme={theme}>{Children}</ThemeProvider>)
